1ba0848dcc5668c64b84ce11d4c2fed2df2aeef0
abodiamhe <akhigbesadiq@gmail.com>
2024-06-23 19:10:45 +0100

    removed env

--- server/index.js
+++ server/index.js
@@ -29,11 +29,11 @@ app.use((req, res, next) => {
 });
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  post: process.env.PG_POST,
+  user: "postgres",
+  host: "localhost",
+  database: "posts",
+  password: "mariam440",
+  post: 5432,
 });
 db.connect();
 
