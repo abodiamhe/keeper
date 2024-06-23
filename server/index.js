c72d57a1df8581ae2ea6b705b917fedeba94ce7c
abodiamhe <akhigbesadiq@gmail.com>
2024-06-23 23:54:05 +0100

    added sql

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import axios from "axios";
 // import cors from "cors";
 import pg from "pg";
 import env from "dotenv";
+import { sql } from "@vercel/postgres";
 
 const app = express();
 const port = 4000;
@@ -28,35 +29,40 @@ app.use((req, res, next) => {
   next();
 });
 
-const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  post: process.env.PG_POST,
-});
-db.connect();
+// const db = new pg.Client({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   post: 5432,
+// });
+// db.connect();
 
 app.get("/posts", async (req, res) => {
-  const result = await db.query("SELECT * FROM posts");
-  res.json(result.rows);
+  try {
+    const result =
+      await sql`CREATE TABLE IF NOT EXISTS posts ( id SERIAL PRIMARY KEY, title varchar(255), content varchar(255) );`;
+    res.status(200);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+  const results = await sql`SELECT * FROM posts`;
+  // const result = await db.query("SELECT * FROM posts");
+  res.json(results.rows);
 });
 
 app.post("/post", async (req, res) => {
   const title = req.body.title;
   const content = req.body.content;
 
-  await db.query("INSERT INTO posts(title, content) VALUES ($1, $2)", [
-    title,
-    content,
-  ]);
+  await sql`INSERT INTO posts(title, content) VALUES (${title}, ${content})`;
   res.status(200).json({ success: true });
 });
 
 app.delete("/delete", async (req, res) => {
   const id = req.body[0];
 
-  await db.query("DELETE FROM posts WHERE id=$1", [id]);
+  await sql`DELETE FROM posts WHERE id=${id}`;
   res.status(200).json({ success: true });
 });
 
